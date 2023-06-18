5f5e3389cdb21ce66a7ec0a121f8f2414cfbfb5a
MohamedEmad <aleslam12345@gmail.com>
2023-06-18 10:32:20 +0300

    setup viewpager with bottom navigation

---
+++ app/src/main/java/com/mo_insta/instagramclone/presentation/profileFragment/ProfileFragment.kt
@@ -0,0 +1,26 @@
+package com.mo_insta.instagramclone.presentation.profileFragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mo_insta.instagramclone.R
+import com.mo_insta.instagramclone.presentation.homeFragment.HomeFragment
+
+class ProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    companion object {
+        fun getInstance(): ProfileFragment {
+            return ProfileFragment()
+        }
+    }
+}
