7cf28202cc916c3cb7f5faadd6842d0b40e2458d
MohamedEmad <aleslam12345@gmail.com>
2023-06-20 20:24:15 +0300

    setOnClicks to items in the collapsing bar

--- app/src/main/java/com/mo_insta/instagramclone/presentation/homeFragment/HomeFragment.kt
+++ app/src/main/java/com/mo_insta/instagramclone/presentation/homeFragment/HomeFragment.kt
@@ -1,17 +1,15 @@
 package com.mo_insta.instagramclone.presentation.homeFragment
 
-import android.content.Context
 import android.os.Bundle
-import android.util.TypedValue
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import com.mo_insta.instagramclone.MainActivity
 import com.mo_insta.instagramclone.R
 import com.mo_insta.instagramclone.databinding.FragmentHomeBinding
+import com.mo_insta.instagramclone.utils.showToast
 
 class HomeFragment : Fragment() {
 
@@ -27,12 +25,22 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setOnClicks()
+    }
 
+    private fun setOnClicks() {
+        val messages = binding.toolbar.findViewById<ImageView>(R.id.iv_messages)
+        messages.setOnClickListener {
+            showToast(requireContext(), "messages")
 
-
-    companion object {
-        fun getInstance(): HomeFragment {
-            return HomeFragment()
+        }
+        val notifications = binding.toolbar.findViewById<ImageView>(R.id.iv_notifications)
+            notifications.setOnClickListener {
+            showToast(requireContext(), "notifications")
         }
     }
+
+
 }
