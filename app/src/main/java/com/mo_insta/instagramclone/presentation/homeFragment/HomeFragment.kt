056bfd91e5d851fea108b6871e53a25a6265ed2f
MohamedEmad <aleslam12345@gmail.com>
2023-06-20 18:28:48 +0300

    add home collapsing toolbar

--- app/src/main/java/com/mo_insta/instagramclone/presentation/homeFragment/HomeFragment.kt
+++ app/src/main/java/com/mo_insta/instagramclone/presentation/homeFragment/HomeFragment.kt
@@ -1,19 +1,34 @@
 package com.mo_insta.instagramclone.presentation.homeFragment
 
+import android.content.Context
 import android.os.Bundle
+import android.util.TypedValue
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.mo_insta.instagramclone.MainActivity
 import com.mo_insta.instagramclone.R
+import com.mo_insta.instagramclone.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        (requireActivity() as MainActivity).setSupportActionBar(binding.myToolbar)
+        (requireActivity() as MainActivity).getSupportActionBar()?.setDisplayShowTitleEnabled(false)
+
+        return binding.root
     }
+
+
+
 
     companion object {
         fun getInstance(): HomeFragment {
